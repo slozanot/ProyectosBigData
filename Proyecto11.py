85dd57ee0d369644876155aa69c250d4c6ec381d
Sergio Lozano <sergio.lozanot@gmail.com>
2016-12-14 15:51:28 +0100

    Add files via upload
    Pasos iniciales del proyecto 11 Contaminacion en Madrid

---
+++ Proyecto11.py
@@ -0,0 +1,61 @@
+import urllib2
+import time
+import math
+
+def obtenerDatos():
+    ficheroSalida = operarFicheros("Escritura", "ficheroContaminacionBruto.txt")
+    ficheroLog = operarFicheros("Escritura", "LogContaminacion.txt")
+    try:
+        url = 'http://datos.madrid.es/datosabiertos/MEDIOAMBIENTE/CALIDAD_DEL_AIRE/2016/10/datos201610.txt'
+        response = urllib2.urlopen(url=url)
+        the_page = response.read()
+        ficheroSalida.writelines(the_page)
+    except:
+        ficheroLog.writelines(time.strftime("%H%M"))
+        ficheroLog.writelines("Error al solicitar los datos")
+    ficheroSalida.close()
+    ficheroLog.close()
+
+def filtrarDatos
+def obtenerMedia(fichero):
+    contador=0
+    suma=0
+    for line in fichero:
+        contador+=1
+        suma+=int(line)
+    return suma/contador
+
+def obtenerVarianza(fichero,media):
+    sumatorio=0
+    contador=0
+    for line in fichero:
+        sumatorio+=math.pow(int(line)-media,2)
+        contador+=1
+    return sumatorio/contador
+
+def operarFicheros(operacion,fichero):
+    try:
+        if operacion == 'Lectura':
+            file = open(fichero,"r")
+        elif operacion == 'Sobreescritura':
+            file = open(fichero,"w")
+        elif operacion == 'Escritura':
+            file = open(fichero,"a")
+        return file
+    except:
+        print "Error al abrir fichero"
+        exit()
+
+obtenerDatos()
+fInput=operarFicheros('Lectura','ficheroContaminacionBruto.txt')
+fOutput=operarFicheros('Escritura','ficheroContaminacion.txt')
+filtrarDatos(28079004,28079038,28079040,fInput,fOutput)
+
+EL FICHERO ESTA COMPUESTO POR:
+CODIGO DE MEDIDOR (PLAZA ESPAÑA, CUATRO CAMINOS O VALLECAS VILLA)
+CODIGO DE MAGNITUD Y DE TECNICA DE MEDIA
+04 QUE INDICA QUE SON DATOS DIARIOS
+AAMM(DESDE ENERO HASTA OCTUBRE, QUE SON LOS DATOS QUE TENEMOS)
+NUMERICO DE 5 DIGITOS Y CODIGO VALIDACION (SI ES V OK, SI ES N NO ES VALIDO -> NO CONTARLO)
+CADA GRUPO NUMERICO+CODIGO VALIDACION REPRESENTA CADA DIA DEL MES
+
